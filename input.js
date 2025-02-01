ba1c6ed88591c1f1f6f8abb66f56a96ef44d27d8
LunarMattrix <ryancg3376@gmail.com>
2025-02-01 15:04:16 -0500

    Create input.js

---
+++ input.js
@@ -0,0 +1,21 @@
+function storeName() {
+let name =document.getElementById("userinput").value;
+document.getElementById("userinput").innerText = "Stored Name: " + name;
+}
+
+function storeEducation() {
+let education = document.getElementById("userinput").value;
+}
+
+function storeProject() {
+let project=document.getElementById("userinput").value;
+}
+
+function storeSkills() {
+let skills=document.getElementById("userinput").value;
+}
+
+function storeWorkExperience() {
+let experience=document.getElementById("userinput").value;
+}
+
